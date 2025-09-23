291922506de27a8dce3d7db361558daa771e72e2
Reinaldo Contreras <reinaldojcg1@gmail.com>
2025-09-23 16:47:01 -0500

    Add slice pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from '../../components/Card/Card.component';
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
 
 const client1 = {
   name: 'Reinaldo',
@@ -18,11 +18,13 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class UncommonPageComponent {
+  // select piope
+
   client = signal(client1);
 
   invitationMap = {
@@ -37,5 +39,21 @@ export default class UncommonPageComponent {
     }
 
     this.client.set(client1);
+  }
+
+  // plural pipe
+
+  clients = signal(['Fernando', 'Reinaldo', 'Diego', 'Genesis', 'Zulimar', 'Maria']);
+
+  // we could handle this map with a normal property but we could also use signals
+  clientMap = signal({
+    '=0': 'no tenemos clientes esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    other: 'tenemos # clientes esperando',
+  });
+
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1));
   }
 }
